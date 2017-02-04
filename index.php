dfd6c9fb7bf8b4434b8440cad45c078961f75753
RedMeca <tytydodo67140@gmail.com>
2017-02-04 15:33:38 +0100

    Début

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="author" content="Tino Dorn"/>
+        <link href="Extension/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="Extension/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+        <link href="Extension/bootstrap-social.css" rel="stylesheet"/>
+        <link href="style.css" rel="stylesheet"/>
+        <title>MisterTrashFood</title>
+    </head>
+    <body class="container-fluid background">
+        <script src="bootstrap-3.3.7-dist/js/jquery-3.1.1.min.js"></script>
+        <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+        <header class="margin-bottom row container-fluid hidden-xs hidden-sm">
+            <nav class=" navbar navbar-inverse navbar-default navbar-fixed-top col-md-12">
+                <div class="row">
+                    <a href="index.html" class="navbar-brand">Mister<span class="brand">Trash</span>Food</a>
+                    <ul class="nav navbar-nav btn-group col-md-4">
+                        <li><a class="btn">Accueil</a></li>
+                        <li><a class="btn">Catégories</a></li>
+                        <li><a class="btn">La Foodtique</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav col-md-2">
+                        <li><a class="navbar-form btn btn-social-icon btn-pinterest"><span class="fa fa-pinterest fa-inverse"></span></a></li>
+                        <li><a class="navbar-form btn btn-social-icon btn-facebook"><span class="fa fa-facebook fa-inverse"></span></a></li>
+                        <li><a class="navbar-form btn btn-social-icon btn-google"><span class="fa fa-google fa-inverse"></span></a></li>
+                        <li><a class="navbar-form btn btn-social-icon btn-instagram"><span class="fa fa-instagram fa-inverse"></span></a></li>
+                    </ul>
+                    <div class="margin1">
+                        <form class="navbar-form navbar-right" role="search">
+                            <div class="input-group">
+                                <input type="search" placeholder="Recherche" class="form-control"/>
+                                <div class="input-group-btn">
+                                    <a class="btn btn-default "><span class="fa fa-search"></span></a>
+                                </div>
+                            </div>
+                            <a class="btn backred"><span class="blanc fa fa-user"></span></a>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <header class="row container-fluid hidden-md hidden-lg">
+            <nav class="navbar navbar-inverse navbar-default navbar-fixed-top col-xs-12">
+                <div class="row">
+                    <a role="button" data-toggle="collapse" href="#collapsed" class="navbar-brand btn bars col-xs-2"><span class="fa fa-bars"></span></a>
+                    <div class="navabar-header col-xs-8">
+                        <a class="navbar-brand navbar-brand1 btn">Mister<span class="brand">Trash</span>Food</a>
+                    </div>
+                    <a class="backred navbar-brand btn col-xs-2"><span class="fa fa-user fa-inverse"></span></a>
+                </div>
+                <div class="collapse" id="collapsed">
+                    <ul class="row nav navbar-nav btn-group col-xs-12">
+                        <li class="col-xs-12"><a class="btn">Accueil</a></li>
+                        <li class="col-xs-12"><a class="btn">Catégories</a></li>
+                        <li class="col-xs-12"><a class="btn">La Foodtique</a></li>
+                    </ul>
+                    <div class="row">
+                        <form class="col-xs-12 margin3" role="search">
+                            <div class="input-group">
+                                <input type="search" placeholder="Recherche" class="form-control"/>
+                                <div class="input-group-btn">
+                                    <a class="btn btn-default "><span class="fa fa-search"></span></a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="backblanc container-fluid row jumbotron">
+            <h1 class="base text-right">Mister<span class="red">Trash</span>Food</h1>
+            <h2 class="italic base text-right">Votre site nutrition, conseil &  révélation version <span class="brandtrash"> TRASH </span></h2>
+        </div>
+        <div class="col-md-8">
+            <div class="row">
+                <div class="well marginleft jumbotron">
+                    <h2 class="base text-center"><a href="#">Huile de palme & huile de colza</a></h2>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-offset-1 col-md-3 well">
+            <h3 class="trash trasheurs text-center">Les Food<span class="trasheur">Trash</span>eurs</h3>
+            <p>Rejoins le clan des Foodtrasheurs</p>
+        </div>
+        <footer class="row hidden-xs hidden-sm">
+            <div class="navbar navbar-inverse navbar-default navbar-fixed-bottom">
+                
+            </div>
+        </footer>
+    </body>
+</html>
